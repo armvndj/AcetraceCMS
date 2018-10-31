de6dd9bbd99bb1f6e7250b61f763cad77c4caa22
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2018-10-31 11:25:02 +0100

    B

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -2,6 +2,6 @@ class Billing < ApplicationRecord
 	enum status: [:unapproved, :approved]
   belongs_to :mycase
   belongs_to :poster, polymorphic: true
-  mount_uploader :attachment, PaymentUploader # Tells rails to use this uploader for this model.
+  mount_uploader :payment, PaymentUploader # Tells rails to use this uploader for this model.
  
 end
