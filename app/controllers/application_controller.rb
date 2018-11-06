e9d502fba7238985dc08a84f3ba4200a8e7c0b32
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2018-11-06 15:41:20 +0100

    gh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :notify
-   #before_action :perform
+   before_action :perform
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   
