80c880a3756c4164b9ae3c295ed3e892acc2565e
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2018-11-13 19:55:01 +0100

    Berd

--- app/controllers/sadmins_controller.rb
+++ app/controllers/sadmins_controller.rb
@@ -1,6 +1,6 @@
 class SadminsController < ApplicationController
   skip_before_action :verify_authenticity_token
-skip_before_action :authenticate_user!
+#skip_before_action :authenticate_user!
 
 	def new
    @user = Sadmin.new
